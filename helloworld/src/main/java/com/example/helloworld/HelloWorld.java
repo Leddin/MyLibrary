8eb4857c79467f8572b585fc2c3f835b87995262
Leddin <leddin@pinscollective.com>
2018-04-05 10:47:57 +0200

    Initial commit

---
+++ helloworld/src/main/java/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.example.helloworld;
+
+import android.util.Log;
+
+/**
+ * Created by Leddin on 5/4/2018.
+ */
+
+public class HelloWorld {
+
+    private static final String TAG = "Hello World";
+
+    public static void Hello(String message){
+        Log.d(TAG, message);
+    }
+}
