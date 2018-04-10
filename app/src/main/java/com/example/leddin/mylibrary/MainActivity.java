4b5df5ee42d442e534d92eac769105d5801233c1
Leddin <leddin@pinscollective.com>
2018-04-10 10:59:14 +0200

    10/04, 11am Update
    Added toast to show message

--- app/src/main/java/com/example/leddin/mylibrary/MainActivity.java
+++ app/src/main/java/com/example/leddin/mylibrary/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.leddin.mylibrary;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.helloworld.HelloWorld;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        HelloWorld.Hello("Hello");
     }
 }
--- helloworld/src/main/java/com/example/helloworld/HelloWorld.java
+++ helloworld/src/main/java/com/example/helloworld/HelloWorld.java
@@ -1,6 +1,8 @@
 package com.example.helloworld;
 
-import android.util.Log;
+        import android.content.Context;
+        import android.util.Log;
+        import android.widget.Toast;
 
 /**
  * Created by Leddin on 5/4/2018.
@@ -10,9 +12,12 @@ public class HelloWorld {
 
     private static final String TAG = "Hello World";
 
-    public static void Hello(){
-        String message = null;
     public static void Hello(String message){
         Log.d(TAG, message);
+    }
+
+    public static void shortToast(Context ctx, String string){
+        Toast.makeText(ctx, string, Toast.LENGTH_SHORT).show();
+
     }
 }
